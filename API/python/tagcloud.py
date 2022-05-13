738f8e9cad9e0e580f7395af44dd28781489533c
MZX00 <muneebrxxx@gmail.com>
2022-05-13 06:21:50 +0500

    Revert "backend"
    This reverts commit 967b3e3423a0081099cdb2a49d101392fe37b461.

--- API/python/tagcloud.py
+++ API/python/tagcloud.py
@@ -25,27 +25,35 @@ def generate_word_cloud(text):
 
 
 def tagcloud():
-    urduletters = "آ أ ا ب پ ت ٹ ث  ج چ ح خ  د ڈ ذ ر ڑ ز ژ  س ش ص ض ط ظ ع غ  ف ق ک گ ل م  ن ں و ؤ ۂ ۃ ء ی ئ ے ۓ ".split()
+    urduletters = "آ أ ا ب پ ت ٹ ث  ج چ ح خ  د ڈ ذ ر ڑ ز ژ  س ش ص ض ط ظ ع غ  ف ق ک گ ل م  ن ں و ؤ ہ ۂ ۃ  ھ ء ی ئ ے ۓ ".split()
     configuration = {
         "language": "Urdu"
     }
     reshaper = ArabicReshaper(configuration=configuration)
 
+    # print(data['tweets'])
+    # tweets_text = str(data['tweets'].tolist())
     tweets_text = " ".join(tweet for tweet in data['tweets'])
+    # print(tweets_text)
+
     text = reshaper.reshape(tweets_text)
     text = get_display(text)
 
+    # width=1200, height=600,
     word_cloud = WordCloud(collocations=False,  background_color='white', stopwords=urduletters,
                            scale=5,
+                           #    min_font_size=10,
                            max_font_size=70,
                            font_path='./python/NotoNaskhArabic-Regular.ttf').generate(text)
     word_cloud.to_file("./data/tagcloud.png")
     plt.axis("off")
+    # plt.imshow(word_cloud, interpolation="bilinear")
+    # plt.imsave("./python/tagcloud.png", word_cloud)
 
 
 if __name__ == "__main__":
     try:
-        generate_word_cloud(text)
+        tagcloud()
         print(True)
     except Exception as e:
         print(False)
